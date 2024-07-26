179238b905819094192e901910b9d77c4e1b6efe
hainweb <hainofficialweb@gmail.com>
2024-07-26 20:17:12 +0530

    Update ChatArea.js

--- src/Components/ChatArea.js
+++ src/Components/ChatArea.js
@@ -32,7 +32,7 @@ function ChatArea() {
     };
     axios
       .post(
-        "http://localhost:8080/message/",
+        "https://dummy-chat-server2.onrender.com/message/",
         {
           content: messageContent,
           chatId: chat_id,
@@ -55,7 +55,7 @@ function ChatArea() {
       },
     };
     axios
-      .get("http://localhost:8080/message/" + chat_id, config)
+      .get("https://dummy-chat-server2.onrender.com/message/" + chat_id, config)
       .then(({ data }) => {
         setAllMessages(data);
         setloaded(true);
